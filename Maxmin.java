0c81014c4f89186bd5f7cbf35ead3084dca6a9d3
PallaviWalve <163240862+PallaviWalve@users.noreply.github.com>
2024-04-02 18:11:36 +0530

    Add files via upload

---
+++ Maxmin.java
@@ -0,0 +1,28 @@
+package assignment;
+public class Maxmin
+
+{
+
+	 public static void main(String[] args)
+	 	{
+		 int temp;
+		 int array[]= {7,5,3,6,8,9,4,2,1,10};
+
+		 for (int i = 0; i< array.length; i++) 
+		 {
+			 for (int j = 1; j < array.length; j++) 
+			 {
+				 if(array[j]>array[i]) 
+				 {
+                    temp=array[i];
+                    array[i]=array[j];
+                    array[j]=temp;
+				 }
+
+			 }
+		 }
+		 System.out.println(" SECOND MINIMUM ELEMENT="+array[2]);
+		 System.out.println(" SECOND MAXIMUM ELEMENT="+array[array.length-1]);
+	}
+
+}
---
+++ Program5.java
@@ -0,0 +1,19 @@
+package assignment;
+
+public class Program5 {
+
+	public static void main(String[] args) {
+		int arr[] = {10, 20, 20, 10, 10, 20, 5, 20};
+
+		for (int i = 0; i < arr.length; i++) 
+		{    int count=0;
+			for (int j = 0; j < arr.length; j++) 
+			{
+				if(arr[i]==arr[j])
+					count++;
+			}
+			System.out.println("the number of = " +arr[i]+ "  occurrence  " +count+ "   in array");
+		}
+	}
+
+}
